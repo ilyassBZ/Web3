14a29e56bf0681a8b0b825c8e5b0016612ae5ee7
ilyass bzitar <bzitarilyass75@gmail.com>
2023-02-03 17:58:13 +0100

    finish test script

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,5 @@
-const hre = require("hardhat")
+const hre = require("hardhat");
+
 
 const main=async()=>{
 	const rsvpContractFactory = await hre.ethers.getContractFactory("Web3RSVP");
@@ -25,15 +26,47 @@ const main=async()=>{
 
 	let eventID=wait.event[0].args.eventID;
 	console.log("Event Id:",eventID);
+
+	//call createNewRSVP() function to RSVP with the 3 accounts we initialized
+	//use the .connect(address) modifier to call the function from an address then deployer(default)
+	//use the deposit variable to pass in the value to send
+	//log the emitted events too
+
+	txn = await rsvpContract.createNewRSVP(eventID,{value:deposit});
+	wait=await txn.wait();
+	console.log("NEW RSVP:",wait.events[0].event,wait.events[0].args);
+
+	txn=await rsvpContract.connect(address1).createNewRSVP(eventID,{value:deposit});
+	wait = await txn.wait();
+	console.log("NEW RSVP:",wait.event[0].event,wait.events[0].args);
+
+	txn = await rsvpContract.connect(address2).createNewRSVP(eventID,{value:deposit});
+	wait = await txn.wait();
+	console.log("NEW RSVP:",wait.event[0].event,wait.events[0].args);
+
+	//confirm all of the RSVP by calling the confirmAllAttendees()
+
+	txn = await rsvpContract.confirmAllAttendees(eventID);
+	wait = await txn.wait();
+	wait.events.forEach((event)=>
+	console.log("CONFIRMED:",event.args.attendeeAddress));
+
+	//we can withdraw the funds only 7 days
+	// this section simulates the passing of 8 days
+	await hre.network.provider.send("evm_increaseTime",[15778800000000]);
+
+	txn = await rsvpContract.withdrawUnclaimedDeposits(eventID);
+	wait = await txn.wait();
+	console.log("WITHDRAWN:",wait.events[0].event,wait.events[0].args);
 };
 
 const runMain = async()=>{
 	try {
 		await main();
-		Process.exit(0);
+		process.exit(0);
 	} catch (error) {
 		console.log(error);
-		Process.exit(1);
+		process.exit(1);
 		
 	}
 };
